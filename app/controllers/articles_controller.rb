674647d32cb8ac16bce3c0847c0ad786e33ac4e1
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-10-15 20:02:27 +0900

    movetoindex

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,4 +26,8 @@ class ArticlesController < ApplicationController
     params.permit(:title, :image, :text)
   end
 
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
 end
