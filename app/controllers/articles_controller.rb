c298306b6b4c7ae8902b9c63ca7708f3540abf5d
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-10-15 20:19:50 +0900

    reflect pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :move_to_index, except: :index
   
   def index
-    @articles = Article.includes(:user).order("created_at DESC")
+    @articles = Article.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
 
   def create
